bb16245cd0934f9b196ee1d7fdea07f82a2bb421
Gisselt <gissotk09>
2022-02-03 18:13:24 -0500

    creada DB tabla teachers

---
+++ TeachersFacture/DataContexts/TeacherContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeachersFacture.Models;
+
+namespace TeachersFacture.DataContexts
+{
+    public class TeacherContext : DbContext
+    {
+        public TeacherContext(DbContextOptions<TeacherContext> options): base(options)
+        {
+        }
+
+        public DbSet<Teacher> TeachersInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+
+            var connectionString = configuration.GetConnectionString("ConexionTest");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ TeachersFacture/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeachersFacture.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        [Required]
+        public string IdentifyNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthDate { get; set; }
+        [Required]
+        public string MoneyPay { get; set; }
+        [Required]
+        public int RateHour { get; set; }
+    }
+}
